2df707b57173542c2883eb64cd1032d4f25cf60b
Stan <120746167+StanPlatoria@users.noreply.github.com>
2026-03-10 16:38:12 +0000

    first auth steps

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+def password=(unhashed_password)
+  self.password_digest = BCrypt::Password.create(unhashed_password)
+end
+
+def authenticate(unhashed_password)
+  BCrypt::Password.new(self.password_digest) == unhashed_password
+end
 end
