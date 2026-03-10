19ffb5ef40e65b69ccb170344294309e2345becc
Stan <120746167+StanPlatoria@users.noreply.github.com>
2026-03-10 22:57:23 +0000

    images added

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -68,3 +68,5 @@ Rails.application.configure do
   # So we don't get the "Cannot render console" message in Gitpod
   config.web_console.whiny_requests = false
 end
+
+config.active_storage.service = :local
